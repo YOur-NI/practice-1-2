3f9cdd1fba0622d08ba60b5de5bd19c46bdd8713
YOur-NI <sudakovnikita2006@gmail.com>
2026-09-18 14:39:22 +0500

    Решил 1 задание

--- src/task1-narrowing.ts
+++ src/task1-narrowing.ts
@@ -2,14 +2,23 @@
 // Если value строка - вернуть "Строка: <value>", если число - "Число: <value>", 
 // если boolean - "Логическое: <value>"
 export function describeValue(value: string | number | boolean): string {
-  // Напишите код здесь
+  if (typeof value === "string") {
+    return `Строка: ${value}`;
+  }
+  if (typeof value === "number") {
+    return `Число: ${value}`;
+  }
+  return `Логическое: ${value}`;
 }
 
 // 2. Сужение через instanceof
 // Если error это Error - вернуть error.message
 // Иначе вернуть "Ошибка: <error>" (приведя error к строке)
 export function formatError(error: Error | string): string {
-  // Напишите код здесь
+  if (error instanceof Error) {
+    return error.message;
+  }
+  return `Ошибка: ${error}`;
 }
 
 // 3. Сужение через оператор in
@@ -20,4 +29,8 @@ type Bird = { fly: () => string };
 // Подсказка: используйте оператор "in" (например, "swim" in animal)
 export function moveAnimal(animal: Fish | Bird): string {
   // Напишите код здесь
+  if ("swim" in animal) {
+    return "Плывет";
+  }
+  return "Летит";
 }
